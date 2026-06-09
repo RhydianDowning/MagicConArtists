93150a47f2379acef5ac12cb77444da658fb81cb
Rhydian Downing <RhydianDowning@gmail.com>
2026-06-09 12:07:22 +0100

    stripped back to just gui

--- gui.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import { execSync } from "child_process";
-import { fileURLToPath } from "url";
-import path from "path";
-
-const root = path.dirname(fileURLToPath(import.meta.url));
-execSync(`npx electron "${path.join(root, "electron", "main.js")}"`, { stdio: "inherit", cwd: root });
